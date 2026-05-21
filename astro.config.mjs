d2c25f7a620802e48d21e07b09713fccf5843321
EJCP3 <euddycabrera03@gmail.com>
2026-05-21 14:21:18 -0400

    fix: configurar adaptador de vercel para ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@astrojs/vue';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    adapter: node({ mode: 'standalone' }),
+    adapter: vercel(),
     integrations: [vue()],
     image: {
         remotePatterns: [
